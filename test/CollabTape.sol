f9c1253289d34eba9e651e3e3e1f294219003cee
kadenzipfel <kaden.zipfel@hotmail.com>
2022-08-12 20:45:00 -0700

    Setup test contract

--- test/CollabTape.sol
+++ test/CollabTape.sol
@@ -4,9 +4,16 @@ pragma solidity 0.8.10;
 import "forge-std/Test.sol";
 
 contract CollabTapeTest is Test {
-    function setUp() public {}
+    CollabTape collabTape;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        collabTape = new CollabTape();
     }
+
+    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns(bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    fallback() external payable {}
+    receive() external payable {}
 }
